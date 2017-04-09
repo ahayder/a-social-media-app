26750dea3de1be62f472c9f044aaab94f94aaa6b
N. M. Ali Hayder <alihayder@mssit.com>
2017-04-09 15:05:07 +0600

    After noon april 9th

--- www/pages/home/post-types/image/image-directive.js
+++ www/pages/home/post-types/image/image-directive.js
@@ -44,13 +44,13 @@ function iController($scope, Constants, $ionicPopover, $localStorage, HomeFactor
     };
 
     vm.likePost = function(){
-        var data = {"type":1,"content_id":vm.post.id,"like_type":1,"post_id":vm.post.id,"tz":vm.user.userTZ};
+        var data = {"type":1,"content_id":vm.post.post.id,"like_type":1,"post_id":vm.post.post.id,"tz":vm.user.userTZ};
 
         HomeFactory.doLike(vm.user.key, data).then(
             function(response){
                 console.log(response);
                 if(response.data.data.data_info.fAction == "1"){
-                    vm.post.likes += 1;
+                    vm.post.post.likes += 1;
                 }
             },function(error){
                 console.log(error);
