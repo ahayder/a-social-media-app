3bf3cf508caf583119a9daf6a28d4d3dc74af479
N. M. Ali Hayder <alihayder@mssit.com>
2017-04-15 11:49:18 +0600

    April 15 mid day

--- www/app/controllers.js
+++ www/app/controllers.js
@@ -1,43 +1,45 @@
 angular.module('freemig.controllers', [])
 
-.controller('AppCtrl', function($scope, $localStorage, $rootScope, Constants, $state, ionicToast, $http) {
+  .controller('AppCtrl', function ($scope, $localStorage, $rootScope, Constants, $state, ionicToast, $http) {
 
-  var vm = this;
+    var vm = this;
 
-  vm.apiurl = Constants.apiurl;
+    vm.apiurl = Constants.apiurl;
 
-  if($rootScope.isLoggedin){
-    vm.user = $localStorage.user;
-  }
+    if ($rootScope.isLoggedin) {
+      vm.user = $localStorage.user;
+    }
 
-  vm.logout = function(){
-    console.log("logout");
-    $http.get(vm.apiurl + "/en/api/v0.1/app/auth/logout/?token="+vm.user.token.key).then(
-      function(success){
-        $localStorage.user = undefined;
-        $rootScope.isLoggedin = false;
-        ionicToast.show("Logout successful!", "top", false, 2000);
-        $state.go("signin");
-      },function(error){
-        ionicToast.show("Error! Please try agin.", "top", false, 2000);
-      }
-    );
-    
-  }
+    vm.logout = function () {
+      console.log("logout");
+      console.log(vm.user);
+      return;
+      $http.get(vm.apiurl + "/en/api/v0.1/app/auth/logout/?token=" + vm.user.key).then(
+        function (success) {
+          $localStorage.user = undefined;
+          $rootScope.isLoggedin = false;
+          ionicToast.show("Logout successful!", "top", false, 2000);
+          $state.go("signin");
+        }, function (error) {
+          ionicToast.show("Error! Please try agin.", "top", false, 2000);
+        }
+      );
+
+    }
 
 
-})
+  })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+  })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
+  });
